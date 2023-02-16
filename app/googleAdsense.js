892c184b6cfea9e9383370691e6414ecc79d170f
yogevzipori <yogevzipori@gmail.com>
2023-02-16 14:38:38 -0800

    fix100

--- app/googleAdsense.js
+++ app/googleAdsense.js
@@ -1,13 +1,10 @@
-export function enableGoogleAdsense() {
+export function GoogleAdsense() {
   const head = document.getElementsByTagName("head")[0];
   const scriptElement = document.createElement(`script`);
   scriptElement.type = `text/javascript`;
   scriptElement.async;
-  scriptElement.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4451534042607297${process.env.NEXT_PUBLIC_ADSENSE_ID}`;
+  scriptElement.src = `src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4451534042607297${process.env.NEXT_PUBLIC_ADSENSE_ID}`;
   scriptElement.crossOrigin = "anonymous";
   head.appendChild(scriptElement);
 }
 
-
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4451534042607297"
-     crossorigin="anonymous"></script>
